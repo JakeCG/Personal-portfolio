011bf7102f97e8b5d5482f7bd1c6f02ad4b07df5
Jake Gowler <jakecgowler@gmail.com>
2022-10-13 14:45:01 +0100

    Making the landing page a little nicer.

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.urls import path
 from django.contrib import admin
 from django.conf.urls.static import static
+import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
